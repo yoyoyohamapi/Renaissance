d7c49059e23cb30b7d808b48dcb33dbf13116a96
yoyoyohamapi <472285740@qq.com>
2014-10-04 08:51:14 +0800

    curlHelp fixed

--- src/Renaissance/CommonBundle/Tools/CurlHelper.php
+++ src/Renaissance/CommonBundle/Tools/CurlHelper.php
@@ -6,12 +6,13 @@ class CurlHelper {
 	protected $access_token;
 	protected $base_url;
 	protected $authed;
-
+	protected $get_header_opt;
 	public function __construct( $access_token,$base_url,$authed){
 		$this->access_token = $access_token;
 		$this->base_url = $base_url;
 		$this->authed = $authed;
 		$this->curl_handler = curl_init();
+		$this->get_header_opt = 'Authorization:Bearer '.$this->access_token;
 		curl_setopt ( $this->curl_handler, CURLOPT_RETURNTRANSFER, 1 );
     		curl_setopt($this->curl_handler,CURLOPT_SSL_VERIFYPEER,$this->authed);
 		curl_setopt($this->curl_handler,CURLOPT_SSL_VERIFYHOST,$this->authed);
@@ -26,8 +27,9 @@ class CurlHelper {
 	}
 
 	public function curlGet($api){
-		$api = $this->base_url.$api."?access_token=" . $this->access_token;
+		$api = $this->base_url.$api;
 		curl_setopt($this->curl_handler,CURLOPT_URL,$api);
+		curl_setopt($this->curl_handler,CURLOPT_HTTPHEADER,array($this->get_header_opt));
 		$response =curl_exec($this->curl_handler);
 		if($response === false)
 			return "Curl Error";
@@ -36,13 +38,14 @@ class CurlHelper {
 
 	// $type = 'POST'|'PUT'|'DELETE'
 	public function curlCustom($api,$post_field,$type){
-		$api = $this->base_url.$api."?access_token=" . $this->access_token;
+		$api = $this->base_url.$api;
 		$post_field = json_encode($post_field);
 		curl_setopt($this->curl_handler,CURLOPT_URL,$api);
 		curl_setopt ( $this->curl_handler, CURLOPT_CUSTOMREQUEST, $type);
     		curl_setopt($this->curl_handler, CURLOPT_HTTPHEADER, array(  
 		            'Content-Type: application/json; charset=utf-8',  
-		            'Content-Length: ' . strlen($post_field))  
+		            'Content-Length: ' . strlen($post_field))  ,
+		 	$this->get_header_opt
 		 );  
     		curl_setopt($this->curl_handler,CURLOPT_POSTFIELDS,$post_field);
 		$response =curl_exec($this->curl_handler);
