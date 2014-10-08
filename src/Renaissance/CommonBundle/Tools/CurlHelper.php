da4a30b7b92eb03327f971aec8337ee210ec9a5a
yoyoyohamapi <472285740@qq.com>
2014-10-08 23:39:12 +0800

    Course show and plaza

--- src/Renaissance/CommonBundle/Tools/CurlHelper.php
+++ src/Renaissance/CommonBundle/Tools/CurlHelper.php
@@ -12,7 +12,7 @@ class CurlHelper {
 		$this->base_url = $base_url;
 		$this->authed = $authed;
 		$this->curl_handler = curl_init();
-		$this->get_header_opt = 'Authorization:Bearer '.$this->access_token;
+		$this->get_header_opt  = 'Authorization:Bearer '.$this->access_token;
 		curl_setopt ( $this->curl_handler, CURLOPT_RETURNTRANSFER, 1 );
     		curl_setopt($this->curl_handler,CURLOPT_SSL_VERIFYPEER,$this->authed);
 		curl_setopt($this->curl_handler,CURLOPT_SSL_VERIFYHOST,$this->authed);
@@ -28,14 +28,13 @@ class CurlHelper {
 
 	public function curlGet($api){
 		$api = $this->base_url.$api;
-		curl_setopt($this->curl_handler,CURLOPT_URL,$api);
 		curl_setopt($this->curl_handler,CURLOPT_HTTPHEADER,array($this->get_header_opt));
+		curl_setopt($this->curl_handler,CURLOPT_URL,$api);
 		$response =curl_exec($this->curl_handler);
-		if($response === false)
+		if($response == false)
 			return "Curl Error";
 		return json_decode($response);
 	}
-
 	// $type = 'POST'|'PUT'|'DELETE'
 	public function curlCustom($api,$post_field,$type){
 		$api = $this->base_url.$api;
@@ -44,12 +43,13 @@ class CurlHelper {
 		curl_setopt ( $this->curl_handler, CURLOPT_CUSTOMREQUEST, $type);
     		curl_setopt($this->curl_handler, CURLOPT_HTTPHEADER, array(  
 		            'Content-Type: application/json; charset=utf-8',  
-		            'Content-Length: ' . strlen($post_field))  ,
-		 	$this->get_header_opt
+		            'Content-Length: ' . strlen($post_field),
+		            $this->get_header_opt
+		            ) 			
 		 );  
     		curl_setopt($this->curl_handler,CURLOPT_POSTFIELDS,$post_field);
 		$response =curl_exec($this->curl_handler);
-		if($response === false)
+		if($response == false)
 			return "Curl Error";
 		return json_decode($response);
 	}
--- src/Renaissance/WebBundle/Controller/CourseController.php
+++ src/Renaissance/WebBundle/Controller/CourseController.php
@@ -1,21 +1,118 @@
 <?php
 
 namespace Renaissance\WebBundle\Controller;
-
+use Renaissance\WebBundle\Controller\BaseController;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class CourseController extends Controller
+class CourseController extends BaseController
 {
     public function indexAction()
     {
-        return $this->render('RenaissanceWebBundle:Course:index.html.twig', array(
-                // ...
-            ));    }
+        $curlHelper=$this->get('curlHelper');
+        $api="courses";
+        //$courses=$curlHelper->curlGet($api.'?include[]=syllabus_body');
+        $request=$this->getRequest();
+        $pageno=$request->query->get('pageno');
+        if($pageno=="")
+            $pageno="1";
+        if($pageno=="1")$hasprevPage=false;
+        else $hasprevPage=true;
+        $prevpgno=$pageno-1;
+        $nextpgno=$pageno+2;
+        //$stastic=$curlHelper->curlGet('courses/2/analytics/activity ');
+        $curCsPage=$curlHelper->curlGet($api."?per_page=6&page=".$pageno);
+        $nextCsPage=$curlHelper->curlGet($api."?per_page=3&page=".$nextpgno);
+        $hasnextPage=!empty($nextCsPage);
+        //var_dump($stastic);
+        //exit();
+        $imgurls=array();
+        foreach ($curCsPage as $key => $value) {
+            $courseimg=$curlHelper->curlGet($api."/".$value->id."/files?search_term=cover");
+            //var_dump($courseimg[0]->url);
+            $imgurls[]=$courseimg[0]->url; 
+                    // exit();
+        }
+        $page=array(
+            'hasnextPage'=>$hasnextPage,
+            'hasprevPage'=>$hasprevPage,
+            'currentPage'=>$pageno,
+            'nextPage'=>$nextpgno,
+            'prevPage'=>$prevpgno
+            );
+
+        $data=array(
+            'page'=>$page,'courses'=>$curCsPage,'imgurls'=>$imgurls
+            );
+        return $this->render('RenaissanceWebBundle:Course:index.html.twig',$data);    
+    }
 
     public function showAction($course_id)
     {
-        return $this->render('RenaissanceWebBundle:Course:show.html.twig', array(
-                // ...
-            ));    }
+        $curlHelper=$this->get('curlHelper');
+        $api="courses/".$course_id;
+        $course=$curlHelper->curlGet($api);        
+        $page=$curlHelper->curlGet($api."/front_page");
+        $students=$curlHelper->curlGet($api."/users?enrollment_type=student");
+        $teachers=$curlHelper->curlGet($api."/users?enrollment_type=teacher");
+        $fileimgs=$curlHelper->curlGet($api."/files?search_term=cover");
+        $lessons=$curlHelper->curlGet($api."/modules?include[]=items");
+        $head_urls=array();
+        foreach ($teachers as $key => $value) {
+            $profile=$curlHelper->curlGet("users/".$value->id."/profile");
+            $head_urls[]=$profile->avatar_url;
+        }
+        $cover=$fileimgs[0]->url;
+        $page->body=substr($page->body, 3,-4);
+        //var_dump($lessons[0]->items[0]->title);
+        //exit();
+        $data=array('course'=>$course,'students'=>$students,'teachers'=>$teachers,
+            'page'=>$page,'heads'=>$head_urls,'cover'=>$cover,"lessons"=>$lessons);
+        return $this->render('RenaissanceWebBundle:Course:show.html.twig', $data);    
+    }
+    public function ajaxAction(){
+
+    }
+
+    public function showMoreAction(){
+        //return $this->createJsonResponse(array("ooo"=>"1111"));
+        $request=$this->getRequest();
+        $courses=$request->get('object');
+        $pageno=$request->query->get('pageno');
+        $curlHelper=$this->get('curlHelper');
+        $api=$courses;
+        //$courses=$curlHelper->curlGet($api.'?include[]=syllabus_body');
+        if($pageno=="")
+            $pageno="1";
+        if($pageno=="1")$hasprevPage=false;
+        else $hasprevPage=true;
+        $prevpgno=$pageno-1;
+        $nextpgno=$pageno+1;
+        //$stastic=$curlHelper->curlGet('courses/2/analytics/activity ');
+        $curCsPage=$curlHelper->curlGet($api."?per_page=3&page=".$pageno);
+        $nextCsPage=$curlHelper->curlGet($api."?per_page=3&page=".$nextpgno);
+        $hasnextPage=!empty($nextCsPage);
+        //var_dump($stastic);
+        //exit();
+        $imgurls=array();
+        foreach ($curCsPage as $key => $value) {
+            $courseimg=$curlHelper->curlGet($api."/".$value->id."/files?search_term=cover");
+            //var_dump($courseimg[0]->url);
+            $imgurls[]=$courseimg[0]->url; 
+                    // exit();
+        }
+         $page=array(
+            'hasnextPage'=>$hasnextPage,
+            'hasprevPage'=>$hasprevPage,
+            'currentPage'=>$pageno,
+            'nextPage'=>$nextpgno,
+            'prevPage'=>$prevpgno
+            );
+        $data=array(
+            'page'=>$page,'courses'=>$curCsPage,'imgurls'=>$imgurls
+            );
+        //var_dump($curCsPage);
+        //exit();
+        return $this->render("RenaissanceWebBundle:Course:plaza_more.html.twig",$data);
+    }
 
 }
