c1fc124a20fbcf71ce03bf8b379de44333f9b1dd
TonySongling <352524676@qq.com>
2014-11-08 09:14:16 +0800

    b

--- src/Renaissance/WebBundle/Controller/DefaultController.php
+++ src/Renaissance/WebBundle/Controller/DefaultController.php
@@ -30,13 +30,19 @@ class DefaultController extends BaseController
     }
 
     public function testAction(){
-<<<<<<< HEAD
-        $test_rest = $this->get('communicationREST');
-        $test_rest->deleteCalEvent(8,"课程取消");
-        //var_dump($reports);
-        return new Response("name");
-=======
->>>>>>> f8d1a7bf64a870cb71cd1f8107b9e9a2925f0817
+              $dbconn = $this->getCanvasConn();
+              $sql ="SELECT password_salt from pseudonyms where user_id=1";
+              if($dbconn){//别忘记是否连接成功
+                            $result = pg_query($dbconn,$sql);
+                            if(!empty($result)){
+                                $alt = pg_fetch_array($result,0);
+                                return new Response($alt[0]);
+                            }else{
+                                return new Response("No Data");
+                            }
+              }else{
+                      return new Response("error");
+             }
     }
 
     public function mtAction(){
